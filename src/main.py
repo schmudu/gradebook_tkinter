6ddc602452b73bf7765edd651a8e493e63a94b53
Patrick Lee <patrick.robin.lee@gmail.com>
2014-10-21 22:10:14 -0700

    Got TKinter working.

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from src.view.main_view import MainView
+import tkinter as tk
+
+if __name__ == "__main__":
+  root = tk.Tk()
+  main_view = MainView(master=root).mainloop()
---
+++ src/view/main_view.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+
+class MainView(tk.Frame):
+  def __init__(self, master):
+    super().__init__(master)
